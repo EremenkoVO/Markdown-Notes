e01928b13bd7ff0acf33221f4ffc4d9c0d4fee42
EremenkoVO <eremeko.v.o@yandex.ru>
2022-11-19 20:12:54 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ListView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListView from './components/ListView.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { ListView },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ListView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-min-6 w-max-8 h-full bg-red">
+    <h3 class="text-medium">Список заметок</h3>
+    <div v-if="isListNotes"></div>
+    <div v-else class="text-center">Сейчас у вас нет заметок</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListView',
+  props: {
+    ListNotes: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    isListNotes() {
+      return this.ListNotes.length > 0;
+    },
+  },
+};
+</script>
