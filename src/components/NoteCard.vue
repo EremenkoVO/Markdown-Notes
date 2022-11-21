3ae486c937b2e177bce8e44c5f97ab8a09f62d68
EremenkoVO <eremeko.v.o@yandex.ru>
2022-11-21 14:35:41 +0300

    Кнопка удалить карточку

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,13 +1,27 @@
 <template>
   <div
-    class="p-4 hw-note-card border-2 border-solid border-blue-light bg-cornflower-blue-50 hover:bg-cornflower-blue-50a cursor-pointer transition ease-in-out delay-150 duration-300"
+    class="p-4 relative hw-note-card border-2 border-solid border-blue-light bg-cornflower-blue-50 hover:bg-cornflower-blue-50a cursor-pointer transition ease-in-out delay-150 duration-300"
+    ref="boardCard"
+    @mouseover="displayXMark(true)"
+    @mouseleave="displayXMark(false)"
   >
-    <p class="text-blue-dark">
-      {{ cutText(text) }}
-    </p>
+    <div v-show="true" class="absolute top-0 right-0">
+      <button
+        class="rounded p-2 rounded-full bg-white text-red drop-shadow-2xl hover:bg-red-lightest"
+      >
+        <font-awesome-icon icon="fa-solid fa-xmark" />
+      </button>
+    </div>
+    <div class="text">
+      <p class="text-blue-dark">
+        {{ cutText(text) }}
+      </p>
+    </div>
   </div>
 </template>
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'NoteCard',
   props: {
@@ -17,6 +31,7 @@ export default {
     },
   },
   setup() {
+    let isVisibleDeleteBtn = ref(false);
     /**
      * Укорачивание текста
      * @param {string} text
@@ -28,8 +43,14 @@ export default {
       return text;
     };
 
+    const displayXMark = (flag) => {
+      isVisibleDeleteBtn.value = flag;
+    };
+
     return {
+      isVisibleDeleteBtn,
       cutText,
+      displayXMark,
     };
   },
 };
